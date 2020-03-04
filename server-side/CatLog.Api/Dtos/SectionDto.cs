67402d4421ce0527e64d6e5356513bbffaf2cad2
杰文 <surbowl@gmail.com>
2020-03-04 17:06:51 +0800

    完成 SectionDto 及其属性映射

---
+++ server-side/CatLog.Api/Dtos/SectionDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CatLog.Api.Dtos
+{
+    public class SectionDto
+    {
+        /// <summary>
+        /// 板块 ID
+        /// </summary>
+        public long Id { get; set; }
+        
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
---
+++ server-side/CatLog.Api/Profiles/SectionProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CatLog.Api.Data.Models;
+using CatLog.Api.Dtos;
+
+namespace CatLog.Api.Profiles
+{
+    /// <summary>
+    /// AutoMapper 针对 Article 的映射关系
+    /// </summary>
+    public class SectionProfile : Profile
+    {
+        public SectionProfile()
+        {
+            /*
+             * CreateMap<源类型,目标类型>();
+             * 属性名称一致时自动赋值
+             * 自动忽略空引用
+            */
+            CreateMap<Section, SectionDto>();
+        }
+    }
+}
